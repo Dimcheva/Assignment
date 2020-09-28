554045aeaef15e5ca029d7313ebbe4273079ef76
irina.dimcheva <irina.dimcheva@primeholding.com>
2020-09-28 16:08:55 +0300

    fluenWait practical

--- src/test/java/fluentWait.java
+++ src/test/java/fluentWait.java
@@ -1,2 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.function.Function;
+
 public class fluentWait {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\myWork\\project assets\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading");
+        driver.findElement(By.linkText("Example 2: Element rendered after the fact")).click();
+        driver.findElement(By.cssSelector("[id=start] button")).click();
+
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class);
+
+        WebElement flw = wait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver webDriver) {
+                if (driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed()) {
+                    return driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]"));
+                } else
+                    return null;
+
+            }
+
+        });
+        System.out.println(driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).getText());
+
+    }
+
 }
