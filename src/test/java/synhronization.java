b5ea8963197546e9f96d225a80da82760181af97
irina.dimcheva <irina.dimcheva@primeholding.com>
2020-09-28 14:21:26 +0300

    refactor

--- src/test/java/synhronization.java
+++ src/test/java/synhronization.java
@@ -1,14 +1,12 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class Synhronization {
+public class synhronization {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\myWork\\project assets\\chromedriver.exe");
